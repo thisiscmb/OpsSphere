b1b7b825efa1ab327fe1a2be04aa6befa7863b69
Chandan Bharadwaj <68054481+thisiscmb@users.noreply.github.com>
2026-07-20 10:08:08 +0530

    feat: dockerize backend application

--- backend/routes/timetrackerRoutes.js
+++ backend/routes/timetrackerRoutes.js
@@ -15,8 +15,8 @@ router.post("/save-activity", timeTrackerController.saveActivity);
 // Check if auto-logout should happen
 // router.post("/check-auto-logout", timeTrackerController.checkAutoLogout);
 
-// Check if early logout reason is required
-router.post("/check-early-logout", timeTrackerController.checkEarlyLogout);
+// // Check if early logout reason is required
+// router.post("/check-early-logout", timeTrackerController.checkEarlyLogout);
 
 // Get current session for a user
 router.get("/current-session/:username", timeTrackerController.getCurrentSession);
